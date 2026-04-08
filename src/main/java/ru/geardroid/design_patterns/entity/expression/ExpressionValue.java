81fe7e46676bc8e60b36f5b704c400af3f0139b2
Vlad Tsatsko <VTsatsko@alfabank.ru>
2026-04-08 21:18:27 +0300

    feat(*): add composite pattern

---
+++ src/main/java/ru/geardroid/design_patterns/entity/expression/ExpressionValue.java
@@ -0,0 +1,21 @@
+package ru.geardroid.design_patterns.entity.expression;
+
+import java.util.List;
+
+public record ExpressionValue(Number value) implements SubExpression {
+
+    @Override
+    public SubExpression add(SubExpression expr) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SubExpression sub(SubExpression expr) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<SubExpression> getExpressionOperations() {
+        throw new UnsupportedOperationException();
+    }
+}
